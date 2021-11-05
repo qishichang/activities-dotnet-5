c7bceb01b21c41a8fef49c0885443a6f1d1c7ed4
Echo Qi <qishichang@live.com>
2021-11-06 00:32:56 +1300

    Fetch activities data from the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,10 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("Default"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +51,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
